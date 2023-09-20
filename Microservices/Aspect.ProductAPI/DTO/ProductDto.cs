b742ad5e3788eccf6b8a0a283cb8f348506f91b4
unknown <tanedochrisjohn@gmail.com>
2023-09-21 00:01:32 +0800

    Add Product Dto

---
+++ Microservices/Aspect.ProductAPI/DTO/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿namespace Aspect.ProductAPI.DTO
+{
+    public class ProductDto
+    {
+      
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public decimal Price { get; set; }
+        public int Quantity { get; set; }
+
+        public string Image { get; set; }
+    }
+}
